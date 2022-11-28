f389fe57d4f2cea202d7d783e2acd132b2ecbe23
mkatlihan <mkatlihan@gmail.com>
2022-11-28 14:24:24 +0300

    fix ver

--- src/lindo_test/test.py
+++ src/lindo_test/test.py
@@ -8,15 +8,15 @@ def test_pyLindo_version():
     import lindo
     import numpy as np
     import os
-
+    #TODO: remove hardcoded version tags, e.g. 14.0 -> LS_IPARAM_VER_MAJOR.LS_IPARAM_VER_MINOR
     pnErrorCode = np.array([-1],dtype=np.int32)
-    licPath = os.getenv('LINDOAPI_HOME')+'/license/lndapi130.lic' 
+    licPath = os.getenv('LINDOAPI_HOME')+'/license/lndapi140.lic' 
     try:
         LicenseKey = np.array([''],dtype='S1024')
         lindo.pyLSloadLicenseString(licPath,LicenseKey)
         pEnv = lindo.pyLScreateEnv(pnErrorCode,LicenseKey)
         lindo.pyLSdeleteEnv(pEnv)
-        print("The Lindo API 13.0 Python interface is working.")
+        print("The Lindo API 14.0 Python interface is working.")
     except lindo.LINDO_Exception as e:
         if(e.args[1] == lindo.LSERR_NO_VALID_LICENSE):
             print(f"{e.args[1]} => Unable to load license at {licPath}")
